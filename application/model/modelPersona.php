39a278e5c9b07dced6fb0a470cba1b3a832bc642
yohitan12 <yhoangaleano@gmail.com>
2015-08-19 17:53:50 -0500

    Cambios con la profe

--- application/model/modelPersona.php
+++ application/model/modelPersona.php
@@ -7,7 +7,6 @@ class ModelPersona
 	public $apellido;
 	public $edad;
 
-
 	public function __GET($key){
 		return $this->$key;
 	}
@@ -42,6 +41,21 @@ class ModelPersona
 	{
 		$sql = "SELECT id, nombre, apellido, edad FROM persona WHERE id=".$id;
 		$query = $this->db->prepare($sql);
+		$query->execute();
+
+        // fetchAll() is the PDO method that gets all result rows, here in object-style because we defined this in
+        // core/controller.php! If you prefer to get an associative array as the result, then do
+        // $query->fetchAll(PDO::FETCH_ASSOC); or change core/controller.php's PDO options to
+        // $options = array(PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC ...
+		return $query->fetchAll();
+	}
+
+
+	public function getPersona2()
+	{
+		$sql = "SELECT id, nombre, apellido, edad FROM persona WHERE id = :id";
+		$query = $this->db->prepare($sql);
+		$query->bindValue(":id", $this->__GET("id"), PDO::PARAM_INT);
 		$query->execute();
 
         // fetchAll() is the PDO method that gets all result rows, here in object-style because we defined this in
